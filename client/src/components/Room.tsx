dfde38cea1fdd4e17ac84d142365ff57f97e75f9
adamdgit <adam_d@outlook.com>
2022-12-21 15:31:39 +1100

    Component for creating/joining rooms

---
+++ client/src/components/Room.tsx
@@ -0,0 +1,45 @@
+
+export default function Room({ setRoom, socket } : {setRoom:React.SetStateAction<string> , socket:any}) {
+
+  // creates and joins a room, name is set by 'room' state value
+  const joinRoom = () => {
+    if (room !== "") {
+      socket.emit("join_room", room)
+    }
+  }
+
+  return (
+    <div className="rooms-wrap">
+
+      <span className="heading1">Find & join rooms</span>
+
+      <section className="create-room">
+      <span className="heading2">Create a room</span>
+        <div>
+          <input className='input-message' 
+            onChange={(e) => setRoom(e.target.value)} 
+            type="text" 
+            placeholder='Create room...' />
+          <button className='send-button' onClick={() => joinRoom()}>Create Room</button>
+        </div>
+      </section>
+
+      <section className="available-rooms">
+      <span className="heading2">Find a room</span>
+        <span className="room">
+          Room1
+          <button className='send-button' onClick={() => joinRoom()}>Join Room</button>
+        </span>
+        <span className="room">
+          Room2
+          <button className='send-button' onClick={() => joinRoom()}>Join Room</button>
+        </span>
+        <span className="room">
+          Room3
+          <button className='send-button' onClick={() => joinRoom()}>Join Room</button>
+        </span>
+      </section>
+
+    </div>
+  )
+}
