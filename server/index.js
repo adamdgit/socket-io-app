d557599b9decbd80ccf41817e2443b9ab9efbbee
adamdgit <adam_d@outlook.com>
2022-12-22 15:07:55 +1100

    Store users connected to rooms using listeners

--- server/index.js
+++ server/index.js
@@ -4,6 +4,8 @@ const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const cors = require("cors");
+app.use(cors());
+
 // setup cors to allow front-end to connect
 const io = new Server(server, {
   cors: {
@@ -12,10 +14,50 @@ const io = new Server(server, {
   }
 });
 
-app.use(cors());
+const users = [];
+
+// keep track of users inside of each room
+function storeUserInfo(id, room) {
+
+  const user = {id, room};
+  // if user already exists in room, remove them from the array
+  users.filter((user, index) => {
+    if (user.id === id) users.splice(index, 1);
+  });
+  users.push(user);
+
+};
+
+// remove disconnected user from array
+function removeDisconnectedUser(id) {
+
+  users.filter((user, index) => {
+    if (user.id === id) users.splice(index, 1);
+  });
+
+}
 
 io.on("connection", (socket) => {
+
   console.log(`user has connected: ${socket.id}`);
+
+  socket.on("join_room", ({ socketID, room }) => {
+    console.log(users);
+    socket.join(room);
+    storeUserInfo(socketID, room);
+
+    // send current users in room to all users
+    io.to(room).emit("roomUsers", {
+      room: room,
+      users: users.filter(user => user.room === room)
+    });
+  });
+
+  // listen for users disconnecting
+  socket.on("disconnect", () => {
+    console.log(socket.id);
+    removeDisconnectedUser(socket.id);
+  })
 
   // listen for message from clients, send back message to room
   socket.on("send_message", (data) => {
@@ -23,11 +65,9 @@ io.on("connection", (socket) => {
     io.to(data.room).emit("receive_message", data);
   });
 
-  socket.on("join_room", (data) => {
-    socket.join(data);
-  });
-
 });
+
+
 
 server.listen(3001, () => {
   console.log("server running on 3001.");
