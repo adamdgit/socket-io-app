15a2adcc0003c6ea4ab07629070c2ec1d2b7cc04
adamdgit <adam_d@outlook.com>
2022-12-23 13:11:37 +1100

    Added available rooms listener

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { io, Manager } from "socket.io-client"
 import Lobby from "./components/Lobby";
+import Room from "./components/Room";
 
 // connect to backend socket on 3001
 const manager = new Manager("http://localhost:3001");
@@ -13,18 +14,17 @@ type socketMessage = {
 function App() {
 
   const [received, setReceived] = useState<string[] | []>([]);
+  const [availableRooms, setAvailableRooms] = useState<string[] | []>([]);
   const [socketID, setSocketID] = useState<string | undefined>(undefined);
-  const [message, setMessage] = useState<string | undefined>(undefined);
-  const [room, setRoom] = useState<string>("n/a");
+  const [room, setRoom] = useState<string>("");
 
   // update message array when receiving a message update from server 
   const messageHandler = (message: socketMessage) => {
     setReceived(received => [...received, message.message])
   }
 
-  // send a message to current room
-  const sendMessage = () => {
-    socket.emit("send_message", { message, room })
+  const getAvailableRooms = (rooms: string[]) => {
+    setAvailableRooms(rooms)
   }
 
   // listen for socket updates
@@ -33,7 +33,8 @@ function App() {
     socket.on("connect", () => setSocketID(socket.id));
     socket.on("disconnect", () => console.log('disconnected'));
     socket.on("receive_message", messageHandler);
-    socket.on("roomUsers", (data) => console.log(data))
+    socket.on("roomUsers", (data) => console.log(data));
+    socket.on("available_rooms", getAvailableRooms);
 
     return () => {
       socket.off("receive_message");
@@ -51,21 +52,19 @@ function App() {
         <p>Room: {room}</p>
       </span>
 
-      <Lobby room={room} setRoom={setRoom} socket={socket} socketID={socketID}/>
+      <Lobby 
+        room={room} 
+        setRoom={setRoom} 
+        socket={socket} 
+        socketID={socketID} 
+        availableRooms={availableRooms}
+      />
 
-      {
-
-      }
-
-      <input className='input-message' 
-        onChange={(e) => setMessage(e.target.value)} 
-        type="text" 
-        placeholder='Message...' />
-      <button className='send-button' onClick={() => sendMessage()}>Send Message</button>
-      <h2>Messages:</h2>
-      <ul>{received.map((message, i) => (
-        <li key={i}>{message}</li>
-      ))}</ul>
+      <Room 
+        room={room} 
+        socket={socket} 
+        received={received} 
+      />
 
     </div>
   )
